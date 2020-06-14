4526347d87f8b0797a766bd5427c9b1263d3de5f
rozina <raniyahnoor9988@gmail.com>
2020-06-14 11:01:41 +0500

    assignment_5 done

---
+++ chapter_1/app.js
@@ -0,0 +1,31 @@
+//1.,2.
+
+  alert("Error!Please enter a valid password");
+ 
+ //3.
+ alert("Welcome to JS Land....\n Happy Coding!");
+
+ //4.
+ alert("Welcome to JS Land ...");
+ alert("Happy Coding ! \n ☐ Prevent this page from additional dialog ");
+
+
+ //5.
+console.log("Hello... i can run JS through my web browser's console");
+
+//6. i used alert in my previous assignment no 4
+
+
+//7.
+alert("welcome to my page");
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter_3/app.js
@@ -0,0 +1,53 @@
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+
+
+var a="I am 15 years old";
+alert(a);
+
+
+
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var visit=14;
+
+ if (visit===14){
+    alert("You have visited this site 14 times");
+}
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+
+var birthyear=2000;
+var num=birthyear.toString();
+document.write("my birth year is "+" "+num+"<br>"+"Data type of my declared data type is number");
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var visitorsName="john doe";
+var m=visitorsName.bold();
+var productTitle="T-SHIRT(s)";
+var quantity=5;
+var b=quantity.toString();
+
+document.write(m+"ordered"+" "+b.bold()+" "+productTitle.bold()+ " "+"on XYZ clothing store");
+
+
+
+
